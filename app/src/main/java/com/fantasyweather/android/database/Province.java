dc40bb4a97a0715a271738738d6978f304142fa5
fantasy-zed <1280055385@qq.com>
2017-07-17 15:58:15 +0800

    加入数据库配置

---
+++ app/src/main/java/com/fantasyweather/android/database/Province.java
@@ -0,0 +1,37 @@
+package com.fantasyweather.android.database;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by ${fantasy} on 2017/7/17.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
