e47525464114e5d48d8e0c3cba2e7deda1c8d175
fantasy-zed <1280055385@qq.com>
2017-07-18 13:48:01 +0800

    加入了后台自动刷新

--- app/src/main/java/com/fantasyweather/android/WeatherActivity.java
+++ app/src/main/java/com/fantasyweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.fantasyweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.fantasyweather.android.gson.Forecast;
 import com.fantasyweather.android.gson.Weather;
+import com.fantasyweather.android.service.AutoUpdateService;
 import com.fantasyweather.android.util.HttpUtil;
 import com.fantasyweather.android.util.Utility;
 
@@ -136,6 +138,8 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.putString("weather",responseText);
                             editor.apply();
                             showWeatherInfo(weather);
+                            Intent intent = new Intent(WeatherActivity.this, AutoUpdateService.class);
+                            startService(intent);
                         }else{
                             Toast.makeText(WeatherActivity.this,"获取天气信息失败", Toast.LENGTH_SHORT).show();
                         }
